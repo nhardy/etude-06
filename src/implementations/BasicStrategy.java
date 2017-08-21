8a3ce13bc7cbbb481031ab61695a3b392d699c62
Nathan Hardy <nhardy@live.com.au>
2017-08-21 16:43:58 +1200

    Make this an eclipse project

---
+++ src/implementations/BasicStrategy.java
@@ -0,0 +1,15 @@
+package implementations;
+
+import forsale.*;
+
+public class BasicStrategy implements Strategy {
+    @Override
+    public int bid(PlayerRecord p, AuctionState a) {
+
+    }
+
+    @Override
+    public Card chooseCard(PlayerRecord p, SaleState s) {
+            
+    }
+}
