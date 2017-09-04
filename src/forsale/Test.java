90a63b4c184084e59682f8653e75d017b9f733f5
Nathan Hardy <nhardy@live.com.au>
2017-09-04 16:47:14 +1200

    Update testing to run game many times with random player order

--- src/forsale/Test.java
+++ src/forsale/Test.java
@@ -7,6 +7,10 @@ package forsale;
 
 import implementations.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;;
 
 /**
  *
@@ -18,7 +22,7 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // A null strategy - never bid, always play your top card.
         Strategy s = new Strategy() {
 
@@ -31,9 +35,9 @@ public class Test {
             public Card chooseCard(PlayerRecord p, SaleState s) {
                 return p.getCards().get(0);
             }
-            
+
         };
-        
+
         // A random strategy - make a random bid up to your amount remaining,
         // choose a rand card to sell.
         Strategy r = new Strategy() {
@@ -47,21 +51,51 @@ public class Test {
             public Card chooseCard(PlayerRecord p, SaleState s) {
                 return p.getCards().get((int) (Math.random()*p.getCards().size()));
             }
-            
+
         };
 
         Strategy basicStrategy = new BasicStrategy();
-        
-        ArrayList<Player> players = new ArrayList<Player>();
-        players.add(new Player("Null 1", s));
-        players.add(new Player("Null 2", s));
-        players.add(new Player("Random 1", r));
-        players.add(new Player("Random 2", r));
-        players.add(new Player("Basic 1", basicStrategy));
-        players.add(new Player("Basic 2", basicStrategy));
-        GameManager g = new GameManager(players);
-        g.run();
-        System.out.println(g.getLog());
+
+        HashMap<String, Integer> wins = new HashMap<String, Integer>();
+
+        for (int n = 0; n < 1000; n++) {
+            ArrayList<Player> players = new ArrayList<Player>();
+
+            players.add(new Player("Null 1", s));
+            players.add(new Player("Null 2", s));
+            players.add(new Player("Random 1", r));
+            players.add(new Player("Random 2", r));
+            players.add(new Player("Basic 1", basicStrategy));
+            players.add(new Player("Basic 2", basicStrategy));
+            Collections.shuffle(players);
+            GameManager g = new GameManager(players);
+            g.run();
+
+            ArrayList<PlayerRecord> finalStandings = g.getFinalStandings();
+            Collections.sort(finalStandings, new Comparator<PlayerRecord>() {
+                @Override
+                public int compare(PlayerRecord pr1, PlayerRecord pr2) {
+                    return pr2.getCash() - pr1.getCash();
+                }
+            });
+            String winner = finalStandings.get(0).getName();
+            wins.put(winner, wins.getOrDefault(winner, 0) + 1);
+        }
+
+        ArrayList<Map.Entry<String, Integer>> sortedWins = new ArrayList<Map.Entry<String, Integer>>();
+        for (Map.Entry<String, Integer> entry : wins.entrySet()) {
+            sortedWins.add(entry);
+        }
+        Collections.sort(sortedWins, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+                return e2.getValue() - e1.getValue();
+            }
+        });
+
+        for (Map.Entry<String, Integer> entry : sortedWins) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 
 }
