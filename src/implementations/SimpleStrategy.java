510b11403e04459e3636a358dae9d0345d0e8a0f
leroy bird <leroybird1@gmail.com>
2017-09-08 12:09:11 +1200

    A tidy up for simple strategy.

--- src/implementations/SimpleStrategy.java
+++ src/implementations/SimpleStrategy.java
@@ -9,20 +9,16 @@ import java.util.Comparator;
 public class SimpleStrategy implements Strategy {
     @Override
     public int bid(PlayerRecord player, AuctionState auction) {
-
+        //Calculate which round this is
         int round = auction.getCardsInDeck().size()/auction.getPlayers().size();
-        log(String.valueOf(round));
-        int max_bid;
-        if (round != 0) {
-             max_bid = player.getCash() / (round+1);
-        } else {
-            max_bid = player.getCash();
-        }
-        if (auction.getCurrentBid()  + 1 >= max_bid ){
+        //Divide the remaining money up by the number of rounds left.
+        int max_bid = player.getCash() / (round+1);
+
+        //Bid if the current bid is smaller than the max_bid.
+        if (auction.getCurrentBid() + 1 <= max_bid ){
             return auction.getCurrentBid() + 1;
         }
         return -1;
-
     }
 
     @Override
